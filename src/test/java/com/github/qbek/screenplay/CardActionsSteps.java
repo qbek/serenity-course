ab2b03992525df70b4a2f5c66b54a50cb52e710f
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2019-01-23 23:22:26 +0100

    Using abilities

---
+++ src/test/java/com/github/qbek/screenplay/CardActionsSteps.java
@@ -0,0 +1,21 @@
+package com.github.qbek.screenplay;
+
+import com.github.qbek.screenplay.abilitites.UseCard;
+import cucumber.api.PendingException;
+import cucumber.api.Transform;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.actors.OnStage;
+
+public class CardActionsSteps {
+    @When("^(\\w+) checks his card balance$")
+    public void carlChecksHisCardBalance(String userName) throws Throwable {
+        // OnStage is a global object. You can access it form anywhere ;)
+        Actor user = OnStage.theActorCalled(userName);
+
+        System.out.println("User card: " + user.usingAbilityTo(UseCard.class).getPan() + ",\n"
+                            + "balance: " + user.usingAbilityTo(UseCard.class).getBalance());
+    }
+
+}
