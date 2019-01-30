71174fa3d96c36fc41fd85a0921674ef8fce55f8
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2019-01-30 22:24:02 +0100

    Ability factories plus - exercise solution

--- src/main/java/com/github/qbek/screenplay/abilitites/AccountFactory.java
+++ src/main/java/com/github/qbek/screenplay/abilitites/AccountFactory.java
@@ -1,20 +1,32 @@
 package com.github.qbek.screenplay.abilitites;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
 
 public class AccountFactory {
 
     private static Faker faker = new Faker();
+    private static MockServerClient mockClient = new MockServerClient("localhost", 8080);
 
     //for now both functions looks quite similar....
-    public static UseAccount useActiveAccount() {
-        return generateAccount();
-    }
+    public static UseAccount useActiveAccount() throws JsonProcessingException {
+        UseAccount account = generateAccount();
+        boolean active = true;
+        setAccountInSystem(account, active);
+        return account;
+    };
 
     //...well, they are the same to be honest
     //but it will change in a moment ;)
-    public static UseAccount useInactiveAccount() {
-        return generateAccount();
+    public static UseAccount useInactiveAccount() throws JsonProcessingException {
+        UseAccount account = generateAccount();
+        boolean inactive = false;
+        setAccountInSystem(account, inactive);
+        return account;
     }
 
     private static UseAccount generateAccount() {
@@ -22,5 +34,18 @@ public class AccountFactory {
                 faker.dragonBall().character(),
                 faker.superhero().power()
         );
+    }
+
+    private static void setAccountInSystem(UseAccount account, boolean isActive) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        int responseCode;
+        if (isActive) {
+            responseCode = 200;
+        } else {
+            responseCode = 404;
+        }
+
+        mockClient.when(HttpRequest.request("/login").withBody(mapper.writeValueAsString(account)))
+                .respond(HttpResponse.response().withStatusCode(responseCode));
     }
 }
