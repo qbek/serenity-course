08e3488ad53847595e1c3fe6fee240127cddf9bd
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2019-02-11 22:42:05 +0100

    Tasks introduction

--- src/test/java/com/github/qbek/screenplay/steps/PreconditionSteps.java
+++ src/test/java/com/github/qbek/screenplay/steps/PreconditionSteps.java
@@ -1,26 +1,19 @@
 package com.github.qbek.screenplay.steps;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
-import com.github.qbek.screenplay.abilitites.CardFactory;
 import com.github.qbek.screenplay.abilitites.UseAccount;
-import com.github.qbek.screenplay.abilitites.UseCard;
-import com.github.qbek.screenplay.facts.CardAccountFact;
 import cucumber.api.java.Before;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actors.Cast;
 import net.serenitybdd.screenplay.actors.OnStage;
-import net.serenitybdd.screenplay.facts.Fact;
+import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
+import net.serenitybdd.screenplay.rest.interactions.Post;
 
-import static com.github.qbek.screenplay.abilitites.AccountFactory.useActiveAccount;
-import static com.github.qbek.screenplay.abilitites.CardFactory.useValidCard;
 import static com.github.qbek.screenplay.facts.CardAccountFact.accountWithCard;
 
 public class PreconditionSteps {
-
-    Faker faker = new Faker();
-
     @Before
     public void setup() {
         OnStage.setTheStage(new Cast());
@@ -28,27 +21,18 @@ public class PreconditionSteps {
 
     @Given("^(\\w+) is a card user with active account$")
     public void carlIsACardUserWithActiveAccount(String name) throws Throwable {
-        // Checks if actor was already created
-        // If not - creates a new one and stores in Cast object
         Actor user = OnStage.theActorCalled(name);
-
-        //now we can add two abilities (or even more)
-        //using just one line...
         user.has(accountWithCard());
+        user.can(CallAnApi.at("http://localhost:8080"));
     }
 
 
     @Given("^(\\w+) is logged in his account$")
     public void heIsLoggedInHisAccount(String name) throws Throwable {
-        // Last used actor has set spotlight on him
-        // can be used in steps without actor name
-//        Actor user = OnStage.theActorInTheSpotlight();
-
-        // same result with using pronun: 'he', 'she'...
         Actor user = OnStage.theActorCalled(name);
-        System.out.println("Actor in the spotlight is: " + user.getName());
-        // Having a correct user allows you to access stored abilities
-        System.out.println("User login: " + user.usingAbilityTo(UseAccount.class).getLogin());
-        System.out.println("User password: " + user.usingAbilityTo(UseAccount.class).getPassword());
+        ObjectMapper mapper = new ObjectMapper();
+        UseAccount credentials = user.usingAbilityTo(UseAccount.class);
+        String reqBody = mapper.writeValueAsString(credentials);
+        user.wasAbleTo(Post.to("/login").with(req -> req.body(reqBody)));
     }
 }
